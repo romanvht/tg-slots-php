2f324e9f0ec8d8d6c0d7d144a5512095777d9c82
romanvht <romanvht@gmail.com>
2025-09-05 20:13:11 +0400

    add lock cron job

--- cron.php
+++ cron.php
@@ -2,9 +2,22 @@
 require_once 'core/config.php';
 require_once 'core/bot.php';
 
-ini_set('display_errors', 0);
-ini_set('log_errors', 1);
-error_reporting(E_ALL);
+ini_set("log_errors", 1);
+ini_set("error_log", __DIR__ . "/cron.log");
+
+$lockFile = __DIR__ . '/cron.lock';
+$lockHandle = fopen($lockFile, 'w');
+
+if (!flock($lockHandle, LOCK_EX | LOCK_NB)) {
+    echo "Скрипт уже выполняется. Выход." . PHP_EOL;
+    exit(1);
+}
+
+register_shutdown_function(function() use ($lockHandle, $lockFile) {
+    flock($lockHandle, LOCK_UN);
+    fclose($lockHandle);
+    unlink($lockFile);
+});
 
 try {
     $bot = new TelegramBot(BOT_TOKEN);
